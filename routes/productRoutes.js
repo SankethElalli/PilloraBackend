f04f327679be765a66f35866a01394cb54ee3c92
SankethElalli <elallisanketh7@gmail.com>
2025-05-09 22:24:58 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,35 +4,33 @@ const productController = require('../controllers/productController');
 const auth = require('../middleware/auth');
 
 router.get('/', (req, res, next) => {
-
   if (req.header('Authorization')) {
     return auth(req, res, () => productController.getProducts(req, res));
   }
-
   return productController.getProducts(req, res);
 });
 
 router.post('/', auth, productController.addProduct);
 router.get('/:id', productController.getProduct);
-
 router.patch('/:id', auth, productController.updateProduct);
 
-router.post('/:id/reviews', async (req, res) => {
+// Add review to product
+router.post('/:id/reviews', auth, async (req, res) => {
   try {
     const Product = require('../models/Product');
     const product = await Product.findById(req.params.id);
     if (!product) {
       return res.status(404).json({ message: 'Product not found' });
     }
-
     const review = {
       _id: new Date().getTime().toString(),
       rating: req.body.rating,
       comment: req.body.comment,
       createdAt: new Date(),
-      userId: req.user ? { name: req.user.name || 'Anonymous' } : { name: 'Anonymous' }
+      userId: req.user
+        ? { name: req.user.name || req.user.email || 'Anonymous' }
+        : { name: 'Anonymous' }
     };
-
     if (!product.reviews) product.reviews = [];
     product.reviews.unshift(review);
     await product.save();
